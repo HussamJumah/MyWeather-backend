f5bd3c5eab069010d0ab6d67dd5e76d05e97740d
Hussam Jumah <hussam.jum@gmail.com>
2019-12-17 02:15:24 -0500

    Finished requesting weather data from API

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const http = require('http');
+const https = require('https')
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
 const User = require('./User');
 const City = require('./City');
 const Comment = require('./Comment');
+const fetch = require('node-fetch');
+const async = require('async');
 
 //needed variables
 const mongodbLink = "mongodb+srv://test:test@myweather-i4dcw.mongodb.net/test?retryWrites=true&w=majority";
+const apiKey = "mfC1Gw-UoAQdiz9W20AWwAH1rpgNT7reD2z3yZxe2Gw"
 
 //needed to connect to mongodb
 let options = {
@@ -112,47 +116,66 @@ let weatherRoute = express.Router()
 
 weatherRoute.get('/:zipcode', (req, res) => {
   let zipcode = req.params.zipcode;
-  let zipcodeQuery = {
-    zipcode:zipcode
-  }
-  let populate = {
-    path: 'comments',
-    model:'Comment',
-    populate:{
-      path:"commenter",
-      model:"User"
+
+  async.parallel({
+    weather: (callback) => {
+      requestWeather(zipcode, (err, weather) => {
+        if (err) {
+          callback(err, null)
+        } else {
+          callback(null, weather)
+        }
+      })
+    },
+
+    comments: (callback) => {
+      let zipcodeQuery = {
+        zipcode:zipcode
+      }
+      let populate = {
+        path: 'comments',
+        model:'Comment',
+        populate:{
+          path:"commenter",
+          model:"User"
+        }
+      }
+      City.findOne(zipcodeQuery).populate(populate).exec((err,city)=>{
+        if (err){
+          callback(err, null)
+        }else{
+          if(!city){
+            let city = City({
+              zipcode:zipcode
+            })
+            city.save((err)=>{
+              if(err){
+                callback(err, null)
+              }else{
+                callback(null, city.comments)
+              }
+            })
+          }else{
+            callback(null, city.comments)
+          }
+        }
+      })
     }
-  }
-  City.findOne(zipcodeQuery).populate(populate).exec((err,city)=>{
-    if (err){
+  }, (err, results) => {
+    if (err) {
       throw err
       res.status = 500
       res.writeHead(500, {'Content-Type':"application/json"})
       res.end("There is an error on our side :(")
-    }else{
-      if(!city){
-        let city = City({
-          zipcode:zipcode
-        })
-        city.save((err)=>{
-          if(err){
-            throw err
-            res.status = 500
-            res.writeHead(500, {'Content-Type':"application/json"})
-            res.end("There is an error on our side :(")
-          }else{
-            res.status = 200
-            res.writeHead(200, {'Content-Type':"application/json"})
-            res.end(JSON.stringify(city))
-          }
-        })
-      }else{
-        res.status = 200
-        res.writeHead(200, {'Content-Type':"application/json"})
-        res.end(JSON.stringify(city))
-      }
+    } else {
+      res.status = 200
+      res.writeHead(200, {'Content-Type':"application/json"})
+      res.end(JSON.stringify(results))
     }
   })
+
+
+
 })
 
 weatherRoute.post('/:zipcode/comment', (req, res) => {
@@ -215,3 +238,25 @@ mongoose.connect(mongodbLink, options)
   console.log("Successfully connected to Mongodb");
   app.server.listen(3001)
 })
+
+function requestWeather(zipcode, callback){
+  let url = `https://weather.ls.hereapi.com/weather/1.0/report.json?product=observation&zipcode=${zipcode}&oneobservation=true&apiKey=mfC1Gw-UoAQdiz9W20AWwAH1rpgNT7reD2z3yZxe2Gw`;
+  let settings = { method: "Get" };
+
+  fetch(url, settings)
+    .then(res => res.json())
+    .then((json) => {
+      callback(null, {
+        city: json.observations.location[0].city,
+        temperature:cToF(json.observations.location[0].observation[0].temperature),
+        description:json.observations.location[0].observation[0].description
+      })
+    })
+    .catch(err => {
+      callback(err, null)
+    })
+}
+
+function cToF(value) {
+  return (value * 9/5) + 32
+}
